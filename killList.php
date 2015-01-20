63e621a83a2de89ebf7f77daa3f52bca9217fea2
Philippe Laban <tingofurro@googlemail.com>
2015-01-20 11:24:44 -0500

    test

--- killList.php
+++ killList.php
@@ -3,7 +3,7 @@ print_r(getKillList());
 function getKillList() {
 	include_once('func.php');
 	$file = file_get_contents("killList.txt");
-	$killList = explode("\r\n", $file);
+	$killList = explode("\n", $file);
 	foreach ($killList as $i => $ele) { $killList[$i] = strToWiki($ele);}
 	return $killList;
 }
