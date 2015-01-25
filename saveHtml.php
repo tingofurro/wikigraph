48d69f9758efbc05609f1214c9a079facb165a1a
Philippe Laban <tingofurro@googlemail.com>
2015-01-25 01:02:41 -0500

    Trial

--- saveHtml.php
+++ saveHtml.php
@@ -4,8 +4,7 @@ set_time_limit(7200);
 $r = mysql_query("SELECT * FROM wg_page WHERE visited=0 ORDER BY id");
 while($re = mysql_fetch_array($r)) {
 	$html = file_get_contents('http://en.wikipedia.org/wiki/'.$re['name']);
-
-	mysql_query("UPDATE wg_page SET html='".$html."', visited='1' WHERE id='".$re['id']."'");
+	mysql_query("UPDATE wg_page SET html='".mysql_real_escape_string($html)."', visited='1' WHERE id='".$re['id']."'");
 }
 
 ?>
