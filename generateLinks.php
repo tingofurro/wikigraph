644cea03d866873d45e19eb7940411831a152ce6
Philippe Laban <tingofurro@googlemail.com>
2015-01-25 22:34:07 -0500

    Tester

--- generateLinks.php
+++ generateLinks.php
@@ -3,7 +3,7 @@ include('init.php');
 set_time_limit(3600);
 mysql_query("TRUNCATE wg_links");
 mysql_query("UPDATE wg_page SET visited=0");
-$r = mysql_query("SELECT * FROM wg_page WHERE visited=0 ORDER BY id");
+$r = mysql_query("SELECT * FROM wg_page WHERE visited=0 ORDER BY id LIMIT 1");
 while($re = mysql_fetch_array($r)) {
 	$dom = new DOMDocument;
 	@$dom->loadHTML($re['html']); $pageNames = array();
@@ -18,5 +18,6 @@ while($re = mysql_fetch_array($r)) {
 			mysql_query("INSERT INTO `wg_links` (`id`, `from`, `to`, `type`) VALUES (NULL, '".$re['id']."', '".$found['id']."', '0');");
 		}
 	}
+echo 'Done with '.$re['id']."<br />";
 }
 ?>
