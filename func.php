d668655bc56034651b7b62ba7f291379d89e186a
Philippe Laban <tingofurro@googlemail.com>
2015-01-26 23:50:35 -0500

    Cleaning repo

--- func.php
+++ func.php
@@ -18,4 +18,11 @@ function topMenu() {
 	</div>
 <?php
 }
+function getKillList() {
+	include_once('func.php');
+	$file = file_get_contents("killList.txt");
+	$killList = explode("[]", $file);
+	foreach ($killList as $i => $ele) { $killList[$i] = strToWiki($ele);}
+	return $killList;
+}
 ?>
--- killList.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getKillList() {
-	include_once('func.php');
-	$file = file_get_contents("killList.txt");
-	$killList = explode("[]", $file);
-	foreach ($killList as $i => $ele) { $killList[$i] = strToWiki($ele);}
-	return $killList;
-}
-?>
