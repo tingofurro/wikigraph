11b4210373b30ffff82559930ef6ebf12ae95b53
root <root@twingo-SATELLITE-PRO-U500.(none)>
2015-03-30 23:38:00 -0400

    dbco

--- dbco.php
+++ dbco.php
@@ -3,5 +3,7 @@ $link = mysql_connect("localhost", "root", "wikigraph");
 mysql_select_db("wikigraph");
 header('Content-Type: text/html; charset=utf-8');
 mysql_set_charset('utf8', $link);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ?>
 
