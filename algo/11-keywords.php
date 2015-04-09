a947e3ce2aab5b15019df66b1c93b1fa22664636
Phil Lab <plaban3@gatech.edu>
2015-04-08 23:30:27 -0400

    Final running version of keyword extract

--- algo/11-keywords.php
+++ algo/11-keywords.php
@@ -16,13 +16,13 @@ $python = wherePython();
 $pyscript = '"'.getDocumentRoot().'/algo/11-articleLoader.py"';
 $param1 = '"'.getDocumentRoot().'"';
 $script = $python.' '.$pyscript.' '.$param1;
-
-$pages = array();
+//echo $script;
 $left = mysql_query("SELECT * FROM wg_page WHERE field!=0 AND keywords=''");
 while($left = mysql_fetch_array($left)) {
+	$pages = array();
 	$f = mysql_query("SELECT * FROM wg_field");
 	while($fi = mysql_fetch_array($f)) {
-		$p = mysql_query("SELECT * FROM wg_page WHERE field=".$fi['id']." ORDER BY (CASE WHEN CHAR_LENGTH(keywords)=0 THEN 0 ELSE 1 END), PR DESC LIMIT 40");
+		$p = mysql_query("SELECT * FROM wg_page WHERE field=".$fi['id']." ORDER BY (CASE WHEN CHAR_LENGTH(keywords)=0 THEN 0 ELSE 1 END), PR DESC LIMIT 10");
 		while($pa = mysql_fetch_array($p)) {
 			$txt = strip_tags(file_get_contents('../data/'.$pa['id'].'.txt'));
 			$txt = preg_replace('#(\r\n?|\n){2,}#', '$1$1', $txt);
@@ -41,7 +41,7 @@ while($left = mysql_fetch_array($left)) {
 	mysql_query($sql);
 
 	emptyFolder('11-texts'); emptyFolder('11-results');
-	echo 'Done with'.count($pages)." pages<br />";
+	echo 'Done with '.count($pages)." pages<br />";
 	ob_flush(); flush();
 	$left = mysql_query("SELECT * FROM wg_page WHERE field!=0 AND keywords=''");
 }
